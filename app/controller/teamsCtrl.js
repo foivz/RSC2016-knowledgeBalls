f2b36b7b75bf57aa68431d1711a2538a379b7e19
Marko Zovko <mazovko@foi.hr>
2016-11-27 00:54:25 +0100

    fix

--- app/controller/teamsCtrl.js
+++ app/controller/teamsCtrl.js
@@ -3,4 +3,17 @@ angular.module('myApp')
     $scope.loggedIn = function(){
         return Auth.loggedIn();
     }
+    $scope.user = Auth.getUser();
+    $scope.teams = [];
+    firebase.database().ref('teams').orderByChild('members/'+$scope.user.uid).once('value', function(teams){
+        var keys = Object.keys(teams.val());
+        var now = Date.now();
+        for(var i=0; i<keys.length; i++){
+            var o = teams.val()[keys[i]];
+            o.key = keys[i];
+            $scope.teams.push(o);
+        }
+        console.log($scope.teams);
+        $scope.$apply();
+    })
 })
