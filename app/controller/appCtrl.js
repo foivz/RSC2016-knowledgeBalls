52cea11db9fa2398e150f3af864fbf49a1f1e7d9
Marko Zovko <mazovko@foi.hr>
2016-11-26 18:16:15 +0100

    update

--- app/controller/appCtrl.js
+++ app/controller/appCtrl.js
@@ -1,5 +1,5 @@
 angular.module('myApp')
-.controller('AppCtrl', function($scope, $firebaseAuth, FirebaseService, Auth){
+.controller('AppCtrl', function($scope, $state, $firebaseAuth, FirebaseService, Auth){
     $scope.user = null;
     $firebaseAuth().$onAuthStateChanged(function(firebaseUser) {
       if (firebaseUser) {
@@ -14,6 +14,7 @@ angular.module('myApp')
     });
     
     $scope.logout = function(){
+        $state.go('home');
         Auth.removeUser();
         $firebaseAuth().$signOut();
     }
