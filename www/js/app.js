af29dbd2f4432aa49d6a3d3daffd50fbb0a8508f
Marko Zovko <mazovko@foi.hr>
2016-11-26 11:48:55 +0100

    update

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('myApp', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -26,48 +26,43 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   $stateProvider
 
     .state('app', {
-    url: '/app',
     abstract: true,
     templateUrl: 'templates/menu.html',
     controller: 'AppCtrl'
   })
-
-  .state('app.search', {
-    url: '/search',
+.state('app.login', {
+    url: '/login',
     views: {
       'menuContent': {
-        templateUrl: 'templates/search.html'
+        templateUrl: 'templates/login.html',
+        controller: 'LoginCtrl'
       }
     }
   })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
+  .state('app.quiz', {
+    url: '/quizes',
     views: {
       'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
+        templateUrl: 'templates/quiz.html'
+      }
+    }
+  })
+  .state('app.teams', {
+    url: '/teams',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/teams.html'
+      }
+    }
+  })
+  .state('app.results', {
+    url: '/results',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/results.html'
       }
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/quizes');
 });
---
+++ www/js/app/controller/loginCtrl.js
@@ -0,0 +1,5 @@
+angular.module('myApp')
+
+.controller('LoginCtrl', ['$scope', function($scope) {
+    $scope.test = 'test';
+}]);
