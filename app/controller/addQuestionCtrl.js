402985000a756ce2756c48983311a8f8055c578e
Marko Zovko <mazovko@foi.hr>
2016-11-27 02:11:11 +0100

    fix

---
+++ app/controller/addQuestionCtrl.js
@@ -0,0 +1,21 @@
+angular.module('myApp')
+.controller('AddQuestionCtrl', function($scope, quiz){
+    $scope.new = {
+        question: '',
+        a: '',
+        b: '',
+        c: '',
+        d: '',
+        correct: ''
+    }
+    
+    $scope.saveQuestion = function(){
+        if($scope.new.question || $scope.new.a || $scope.new.b || $scope.new.c || $scope.new.d || $scope.new.correct){
+            $scope.error = 'You haven\'t entered everything';
+            return;
+        }  
+        $scope.error = '';
+        firebase.database().ref('questions/'+id).push($scope.new);
+        $scope.$close();
+    };
+})
--- app/controller/quizShowCtrl.js
+++ app/controller/quizShowCtrl.js
@@ -1,5 +1,5 @@
 angular.module('myApp')
-.controller('QuizShowCtrl', function($scope, Auth, $stateParams){
+.controller('QuizShowCtrl', function($scope, Auth, $stateParams, $uibModal){
     var id = $stateParams.id;
     console.log($stateParams);
     $scope.loggedIn = function(){
@@ -8,6 +8,7 @@ angular.module('myApp')
     $scope.quiz = null;
     $scope.user = Auth.getUser();
     $scope.teams = [];
+    $scope.questions = [];
     firebase.database().ref('quiz/'+id).once('value', function(quiz){
         $scope.quiz = quiz.val();
         firebase.database().ref('users/'+$scope.quiz.moderator).once('value', function(mod){
@@ -16,5 +17,31 @@ angular.module('myApp')
         })
         console.log($scope.quiz);
         $scope.$apply();
+    });
+    
+    $scope.isOwner = function(){
+        if($scope.quiz == null){return false;}
+        if($scope.quiz.moderator == Auth.getUser().uid){
+            return true;
+        }
+        return false;
+    }
+    
+    $scope.addQuestion = function(){
+        if(!$scope.isOwner()){
+            return false;
+        }
+        $uibModal.open({
+          templateUrl: 'templates/addQuestion.html',
+          size: 'sm',
+          controller: 'AddQuestionCtrl',
+          resolve: {
+              quiz: id
+          }
+        });
+    }
+    
+    firebase.database().ref('questions/'+id).on('child_added', function(quest){
+        $scope.questions.push(quest.val());
     })
 })
