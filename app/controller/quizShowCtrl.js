66ef3af2489fce438dcd0c539ee8539ac94ab78e
Marko Zovko <mazovko@foi.hr>
2016-11-27 01:35:51 +0100

    Useless update

--- app/controller/quizShowCtrl.js
+++ app/controller/quizShowCtrl.js
@@ -1,6 +1,7 @@
 angular.module('myApp')
 .controller('QuizShowCtrl', function($scope, Auth, $stateParams){
     var id = $stateParams.id;
+    console.log($stateParams);
     $scope.loggedIn = function(){
         return Auth.loggedIn();
     }
@@ -9,5 +10,11 @@ angular.module('myApp')
     $scope.teams = [];
     firebase.database().ref('quiz/'+id).once('value', function(quiz){
         $scope.quiz = quiz.val();
+        firebase.database().ref('users/'+$scope.quiz.moderator).once('value', function(mod){
+            $scope.quiz.user = mod.val();
+            $scope.$apply();
+        })
+        console.log($scope.quiz);
+        $scope.$apply();
     })
 })
